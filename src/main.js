25124d7940a138f211924129c14e203861db32c6
Rodrigo <rcbm539@gmail.com>
2017-09-05 15:11:11 -0300

    Testando calculadora

---
+++ src/main.js
@@ -0,0 +1,19 @@
+module.exports = {
+
+  sum: function(){
+
+  },
+
+  sub: function(){
+
+  },
+
+  mult: function(){
+
+  },
+
+  div: function(){
+
+  }
+
+}
--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,5 +1,37 @@
-import { expect } from 'chai';
+// import { expect } from 'chai';
 
-describe('Main', () => {
+var expect = require('chai').expect;
 
+var calc = require('../src/main.js');
+
+describe('Calc', () => {
+
+  //smoke tests
+  // Verifica se existe calc, e se dentro de
+  //calc, existe as operações.
+  describe('Smoke tests', function(){
+    it('should exist the calc lib', function(){
+      expect(calc).to.exist;
+    });
+
+    it('should exist the method `sum`', function(){
+      expect(calc.sum).to.exist;
+      expect(calc.sum).to.be.a.function;
+    });
+
+    it('should exist the method `sub`', function(){
+      expect(calc.sub).to.exist;
+      expect(calc.sub).to.be.a.function;
+    });
+
+    it('should exist the method `mult`', function(){
+      expect(calc.mult).to.exist;
+      expect(calc.mult).to.be.a.function;
+    });
+
+    it('should exist the method `div`', function(){
+      expect(calc.div).to.exist;
+      expect(calc.div).to.be.a.function;
+    });
+  });
 });
